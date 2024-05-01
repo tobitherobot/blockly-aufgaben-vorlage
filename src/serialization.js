f6c975aeefe4d565e335dd08c88225fe12814a5d
tobitherobot <baumann.tobias@outlook.com>
2024-05-01 19:51:06 +0200

    Startprogramm hinzugefügt

--- src/serialization.js
+++ src/serialization.js
@@ -22,8 +22,9 @@ export const save = function(workspace) {
  * @param {Blockly.Workspace} workspace Blockly workspace to load into.
  */
 export const load = function(workspace) {
+  const initial = '{"blocks":{"languageVersion":0,"blocks":[{"type":"print_text","id":"Y=DNY!R3o%N!h~1*2GUQ","x":238,"y":111,"inputs":{"VALUE":{"block":{"type":"text","id":"p1$B3D1o~R)}BxR!xQfD","fields":{"TEXT":"Es funktioniert!"}}}}}]},"variables":[{"name":"count","id":"n?kT@:w.=U*R8$T7pUqf"}]}';
   const data = window.localStorage?.getItem(storageKey);
-  if (!data) return;
+  if (!data) data = initial;
 
   // Don't emit events during loading.
   Blockly.Events.disable();
